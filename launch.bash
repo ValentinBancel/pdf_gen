ac6c2036d031bc835390f5e970e85f10f819ac54
valentinbancel <valentin.bancel@orange.fr>
2023-11-24 14:25:45 +0100

    add bash file

---
+++ launch.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Demander à l'utilisateur de saisir un chemin
+echo "Veuillez saisir le chemin jusqu'au fichier python : "
+read path_file
+
+# Afficher le chemin saisi par l'utilisateur
+python3 $path_file
