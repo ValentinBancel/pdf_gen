5b129b0ac2d357e6665f13e2b86da9307d9d3a3e
ValentinBancel <90621647+ValentinBancel@users.noreply.github.com>
2024-03-02 20:24:43 +0100

    add(clean): clean file

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Demander à l'utilisateur de saisir un chemin
-echo "Veuillez saisir le chemin jusqu'au fichier python : "
-read path_file
-
-# Afficher le chemin saisi par l'utilisateur
-python3 $path_file
